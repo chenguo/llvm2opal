7a7010cfefc5fa221626168e6eeb462d1b491fa4
Chen Guo <chenguo4@yahoo.com>
2010-02-28 02:17:59 -0800

    Refactor some globals in l2o.h to new header.
    src/args.h: new file. Holds arguments constants.
    src/l2o.c: include args.h.
    src/l2o.h: remove arguments constants.

---
+++ src/args.h
@@ -0,0 +1,26 @@
+#ifndef ARG_H
+#define ARG_H
+static char arg1[8] = "SOURCE1";
+static char arg2[8] = "SOURCE2";
+static char arg3[8] = "SOURCE3";
+static char arg4[8] = "SOURCE4";
+static char arg5[8] = "SOURCE5";
+static char arg6[8] = "SOURCE6";
+static char arg7[8] = "SOURCE7";
+static char arg8[8] = "SOURCE8";
+static char argi1[6] = "S1ORI";
+static char argi2[6] = "S2ORI";
+static char argi3[6] = "S3ORI";
+static char argi4[6] = "S4ORI";
+static char argi5[6] = "S5ORI";
+static char argi6[6] = "S6ORI";
+static char argi7[6] = "S7ORI";
+static char argi8[6] = "S8ORI";
+char *args[8] = {arg1, arg2, arg3, arg4,
+                        arg5, arg6, arg7, arg8};
+char *argsi[8] = {argi1, argi2, argi3, argi4,
+                         argi5, argi6, argi7, argi8};
+char arg_true[5] = "true";
+char arg_false[6] = "false";
+
+#endif
--- src/l2o.c
+++ src/l2o.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "args.h"
 #include "l2o.h"
 #include "bufops.h"
 
